ee7db8d16d53989fdfc53a362efc9d4389769015
MuriloGalache <117782216+MuriloGalache@users.noreply.github.com>
2023-03-22 18:41:32 -0300

    Add files via upload

---
+++ EX.EBDD.2.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int anos[] = {1, 2, 3, 4};
+  
+    
+    printf("numero vetor: %p, Valor da variavel: %d",&anos[0], anos[0]);
+    
+    return 0;
+}
