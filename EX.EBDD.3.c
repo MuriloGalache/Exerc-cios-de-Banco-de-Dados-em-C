db1b1f932c548a3f3d7c3443bde70eef27a59007
MuriloGalache <117782216+MuriloGalache@users.noreply.github.com>
2023-03-22 20:28:04 -0300

    Add files via upload

---
+++ EX.EBDD.3.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("Olá, seja bem vindo, esses são seus dados estudantis:\n");
+    int salaAluno = 420;
+    char turmaAluno = 'a';
+    float mediaAluno = 6.9;
+    char nomeAluno[6] = Murilo"; 
+    
+    printf("Sua sala é: %d \n", salaAluno );
+    printf("Sua turma é: %c \n", turmaAluno);
+    printf("Sua média é: %f \n", mediaAluno);
+    printf("Seu nome é: %s ", nomeAluno);
+    
+    return 0;
+}
